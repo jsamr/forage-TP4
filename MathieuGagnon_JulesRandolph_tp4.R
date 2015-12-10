2d925ad872fc8e4780604a9bd4393fb3b59fc57a
Mathieu Gagnon <Mathieu Gagnon>
2015-12-09 21:12:08 -0500

    utilisation de kmeans de R avec echantillon de 10 lignes par professeur

--- MathieuGagnon_JulesRandolph_tp4.R
+++ MathieuGagnon_JulesRandolph_tp4.R
@@ -1,1 +1,14 @@
-mydata = read.csv("turkiye-student-evaluation_generic.csv")
+mydata = as.matrix(read.csv("turkiye-student-evaluation_generic.csv"))
+
+dataWOids <- mydata[,3:33]
+
+dataProf1 <- dataWOids[1:10,]
+dataProf2 <- dataWOids[801:810,]
+dataProf3 <- dataWOids[2301:2310,]
+
+dataEchantillon <- rbind(dataProf1,dataProf2,dataProf3)
+dataEchantillon
+
+dist(dataEchantillon, method = "euclidean")
+
+result <- kmeans(dataEchantillon, 3)
